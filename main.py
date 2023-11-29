a02d2a2020835db4d8a721e7dc43f36e7f709aee
MishaChornyi <152193247+MishaChornyi@users.noreply.github.com>
2023-11-29 17:26:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 #class Studend:
 
-def print_hi(name):
+ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
